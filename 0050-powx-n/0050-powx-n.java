d019b831041c8897b3fecbc05b93ab7d2477f2dc
Mohini Sahoo <mohinisahoo395@gmail.com>
2026-08-20 22:32:03 +0530

    Time: 0 ms (100%), Space: 48 MB (57.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long pow=n;
+        double ans=1;
+
+        if(pow<0){
+            x=1/x;
+            pow=-pow;
+        }
+        while(pow>0){
+            if(pow%2==1){
+                ans*=x;
+            }
+            x=x*x;
+            pow=pow/2;
+        }
+        return ans;
+    }
+}
